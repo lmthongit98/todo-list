70f53064d4d1016d59b81c776e3e51841b4071b9
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-03-27 17:22:07 +0700

    Code todoService

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,66 @@
+import { LocalStorageService } from './local-storage.service';
+import { Filter } from './../models/filtering.model';
+import { Todo } from './../models/todo.model';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  private static readonly TodoStorageKey = 'todos';
 
-  constructor() { }
+  private todos!: Todo[];
+  private filteredTodos!: Todo[];
+  private lengthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(
+    0
+  );
+  private displayTodoSubject: BehaviorSubject<Todo[]> = new BehaviorSubject<
+    Todo[]
+  >([]);
+  private currentFilter: Filter = Filter.All;
+
+  todos$: Observable<Todo[]> = this.displayTodoSubject.asObservable();
+  length$: Observable<number> = this.lengthSubject.asObservable();
+
+  constructor(private storageService: LocalStorageService) {}
+
+  fetchFromLocalStorage() {
+    this.todos =
+      this.storageService.getValue<Todo[]>(TodoService.TodoStorageKey) || [];
+    this.filteredTodos = [...this.todos.map((todo) => ({ ...todo }))]; // deep clone
+    this.updateTodosData();
+  }
+
+  updateToLocalStorage() {
+    this.storageService.setObject(TodoService.TodoStorageKey, this.todos);
+    this.filterTodos(this.currentFilter, false);
+    this.updateTodosData();
+  }
+
+  filterTodos(currentFilter: Filter, isFiltering: boolean) {
+    this.currentFilter = currentFilter;
+    
+    switch (currentFilter) {
+      case Filter.Active:
+        this.filteredTodos = this.todos.filter((todo) => !todo.isCompleted);
+        break;
+
+      case Filter.Completed:
+        this.filteredTodos = this.todos.filter((todo) => todo.isCompleted);
+        break;
+      case Filter.All:
+        this.filteredTodos = [...this.todos.map((todo) => ({ ...todo }))];
+        break;
+    }
+
+    if (isFiltering) {
+      this.updateTodosData();
+    }
+  }
+
+  private updateTodosData() {
+    this.displayTodoSubject.next(this.filteredTodos);
+    this.lengthSubject.next(this.todos.length);
+  }
 }
