e836520c113171cfec90c78d52b5a056a04549ba
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2022-03-27 16:12:13 +0700

    setup UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoInputComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
